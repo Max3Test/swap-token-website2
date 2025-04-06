6a54bdd57ce25393624ba6d4a264abeb7839d26c
Max3Test <ChasityRanda08819@outlook.com>
2025-04-06 22:00:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -47,8 +47,8 @@ async function stakeTokens() {
   if (!amount || amount <= 0) return alert("Enter a valid amount");
 
   try {
-    const tokenAddress = ""; // Укажи адрес MAX
-    const wrapperAddress = ""; // Укажи адрес StMAX
+    const tokenAddress = "0x69b4086C7B131ED691d428e2BBa7cAcD4A4C641e"; // Укажи адрес MAX
+    const wrapperAddress = "0x1cC6d610c190C7742FE7603987aBCa76e403CD0d"; // Укажи адрес StMAX
     const tokenABI = ["function approve(address spender, uint256 amount) external returns (bool)"];
     const wrapperABI = ["function deposit(uint256 amount) external"];
     const token = new ethers.Contract(tokenAddress, tokenABI, signer);
@@ -70,7 +70,7 @@ async function unstakeTokens() {
   if (!amount || amount <= 0) return alert("Enter a valid amount");
 
   try {
-    const wrapperAddress = ""; // Укажи адрес StMAX
+    const wrapperAddress = "0x1cC6d610c190C7742FE7603987aBCa76e403CD0d"; // Укажи адрес StMAX
     const wrapperABI = ["function withdraw(uint256 amount) external"];
     const wrapper = new ethers.Contract(wrapperAddress, wrapperABI, signer);
     const value = ethers.utils.parseUnits(amount, 18);
